35b5f701d97110bf5cde33ff6473b422a2fa0dea
Oleksandra Kukharska <93227295+alpascali@users.noreply.github.com>
2025-04-15 20:46:38 +0100

    Update script.js

--- script.js
+++ script.js
@@ -47,3 +47,11 @@ function filterProjects(category) {
         }
     });
 }
+
+//INTERESTS
+document.querySelectorAll('.accordion-header').forEach(header => {
+    header.addEventListener('click', () => {
+        const item = header.parentElement;
+        item.classList.toggle('active');
+    });
+});
